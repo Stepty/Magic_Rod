4e6c1e2f6e38224955d8a2900efb445aabe8bf45
Stepty <stephen7ni@gmail.com>
2024-04-13 02:31:33 -0500

    initial

---
+++ magic-rod/src/components/GraphHolder.js
@@ -0,0 +1,7 @@
+export default function GraphHolder() {
+  return (
+    <div className="bg-white p-4 rounded shadow-lg">
+      {/* Your content here */}
+    </div>
+  );
+}
--- magic-rod/src/pages/index.js
+++ magic-rod/src/pages/index.js
@@ -1,9 +1,15 @@
 
+
 export default function Home() {
   return (
-    <main>
-      <div >
-        <h1>Home</h1>
+    <main class="p-8">
+      <div class="text-center" >
+        <h1>Magic Rod</h1>
+      </div>
+      <div class="flex justify-center">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          On
+        </button>
       </div>
     </main>
   );
