a57dd1bc761093109fae9f4fab22a734083a0ee0
Stepty <stephen7ni@gmail.com>
2024-04-13 06:30:15 -0500

    Revert "add graph button subwaysurfers db connection"
    This reverts commit 45d27dd3de8d71d611d54fc21db66b496e25fd42.

---
+++ magic-rod/src/pages/index.js
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+  return (
+    <main class="p-8">
+      <div class="text-center" >
+        <h1>Magic Rod</h1>
+      </div>
+      <div class="flex justify-center">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          On
+        </button>
+      </div>
+    </main>
+  );
+}
